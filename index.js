12016fa0f3b60c4192bbbce43235ea972f0fa6b7
Aiman <aimanxtech@gmail.com>
2022-08-31 04:31:15 +0600

    working on header

--- index.js
+++ index.js
@@ -8,40 +8,38 @@ const btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-const sitePlan = document.querySelector('.sitePlan')
+const sitePlan = document.querySelector(".sitePlan");
 const fetchData = () => {
-
-    fetch("http://127.0.0.1:5500/deerJS/data.js")
-        .then(res => res.json())
-        .then(data => viewData(data))
-}
+  fetch("http://127.0.0.1:5500/deerJS/data.js")
+    .then((res) => res.json())
+    .then((data) => viewData(data));
+};
 
 const viewData = (data) => {
-
-    data.map(user => {
-        sitePlan.addEventListener("click", (e) => {
-            if (parseInt(e.target.id) == user.id) {
-                console.log(user)
-                // When the user clicks the units, open the modal 
-                modal.style.display = "block";
-                modalContent.innerHTML = user.name + " " + user.email
-                // modalContent.style.top = "50%";
-            }
-        })
-    })
-}
+  data.map((user) => {
+    sitePlan.addEventListener("click", (e) => {
+      if (parseInt(e.target.id) == user.id) {
+        console.log(user);
+        // When the user clicks the units, open the modal
+        modal.style.display = "block";
+        modalContent.innerHTML = user.name + " " + user.email;
+        // modalContent.style.top = "50%";
+      }
+    });
+  });
+};
 
 // When the user clicks on <span> (x), close the modal
 span.addEventListener("click", () => {
-    modal.style.display = "none";
-})
+  modal.style.display = "none";
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
+fetchData();
 
-fetchData()
