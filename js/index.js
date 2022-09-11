49f858b3eee44ce177422106e254e26b1d760ffe
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-09-11 09:21:10 +0600

    revisions done

--- js/index.js
+++ js/index.js
@@ -36,17 +36,64 @@ const viewData = (data) => {
         // When the unit clicks the units, open the modal
         modal.style.display = "block";
         siteplanModalDetails.innerHTML = `
-            <img src=${unit.Img_floorplan} />
-            <div class="siteplan__modal-contents">
-                <h2>${unit.Unit}</h2>
-                <p><i class='bx bx-home'></i> <span>${unit.sqft} Sqft</span></p>
-                <p><i class='bx bx-bed'></i> <span>${unit.Bedrooms} Bedrooms</span></p>
-                <p><i class='bx bx-bath' ></i> <span>${unit.Bathroom} Bathrooms</span></p>
-                <p><i class='bx bx-home-heart' ></i> <span>${unit.Powder_Room} Powder Rooms</span></p>
-                <p><i class='bx bx-building'></i></i> <span>${unit.pow}</span></p>
-            </div>
+                <div class="swiper__js">
+                <div
+                style="
+                  --swiper-navigation-color: var(--primary__color);
+                "
+                class="swiper mySwiper2"
+              >
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <img src=${unit.Img_floorplan}>
+                  </div>
+                  <div class="swiper-slide">
+                    <img src=${unit.Img_blockplan} />
+                  </div>
+                </div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+              </div>
+              <div thumbsSlider="" class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide thumbnail__slide">
+                    <img src=${unit.Img_floorplan}>
+                  </div>
+                  <div class="swiper-slide thumbnail__slide">
+                    <img src=${unit.Img_blockplan} />
+                  </div>
+                </div>
+              </div>
+      
+                </div>
+                <div class="siteplan__modal-contents">
+                    <h2>${unit.Unit}</h2>
+                    <p><i class='bx bx-home'></i> <span>${unit.sqft} Sqft</span></p>
+                    <p><i class='bx bx-bed'></i> <span>${unit.Bedrooms} Bedrooms</span></p>
+                    <p><i class='bx bx-bath' ></i> <span>${unit.Bathroom} Bathrooms</span></p>
+                    <p><i class='bx bx-home-heart' ></i> <span>${unit.Powder_Room} Powder Rooms</span></p>
+                    <p><i class='bx bx-building'></i></i> <span>${unit.pow}</span></p>
+                </div>
         `;
       }
+
+      // swiper js
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
     });
   });
   modalEvents();
